5557d0ca9884bda2cb60ab3c68811cb93ee311c9
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2023-10-25 13:33:28 +0700

    Изменен код анкеты (модификация по модулю 4)

---
+++ Из модуля 3_v1/AssignmentsFromTheWorkshopMod 4/AssignmentsFromTheWorkshopMod 4/Program.cs
@@ -0,0 +1,35 @@
+﻿// 4.5.1
+(string Name, string SurName, string Login, int LoginLength, bool Pet, double AgeUser, string[] ColorUser) User;
+string PetUser;
+
+//4.5.2
+Console.Write("Введите свое имя: ");
+User.Name = Console.ReadLine();
+
+Console.Write("Введите свою фамилию: ");
+User.SurName = Console.ReadLine();
+
+Console.Write("Придумайте свой логин: ");
+User.Login = Console.ReadLine();
+//4.5.3
+User.LoginLength = User.Login.Length;
+
+//4.5.4
+Console.Write("Есть у вас животные? Да или Нет: ");
+PetUser = Console.ReadLine();
+
+if (PetUser == "Да")
+{
+    User.Pet = true;
+    Console.WriteLine(User.Pet);
+    
+    if (PetUser == "Нет")
+    {
+        User.Pet = false;
+        Console.WriteLine(User.Pet);
+    }  
+}
+else
+{
+    Console.WriteLine("Ответьте пожалуйста на вопрос только Да или НЕТ");
+}
